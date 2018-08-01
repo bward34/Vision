a5ed211d3b02f55e677b644f9166a7db78d68d00
bward34 <brandon.ward@utah.edu>
2018-07-31 20:34:22 -0600

    continue build out on home controller

--- Vision/HomeController.swift
+++ Vision/HomeController.swift
@@ -8,16 +8,33 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class HomeController: UIViewController {
+class HomeController: UIViewController, CLLocationManagerDelegate {
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    let API = "30c2075ee7aed4c338fa76abed5e1b3c"
+    
+    let locationManager = CLLocationManager();
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+        locationManager.requestWhenInUseAuthorization();
+        locationManager.stopUpdatingLocation();
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        <#code#>
     }
 }
