d631d6f6b75f4d80fde2769449679c4def80028f
bward34 <brandon.ward@utah.edu>
2018-10-13 16:57:26 -0600

    cooridnates are now recording

--- Vision/CanvasController.swift
+++ Vision/CanvasController.swift
@@ -5,14 +5,20 @@
 //  Created by Brandon Ward on 10/8/18.
 //  Copyright © 2018 LEDBoyzz. All rights reserved.
 //
+// Code based on this tutorial:
+// https://www.raywenderlich.com/6721-welcome-to-the-new-raywenderlich-com
 
 import UIKit
 
 class CanvasController: UIViewController {
     
+    var CANVAS_DATA: [UIColor: [CGPoint]] = [:]
+    var COLOR: UIColor = UIColor.black;
+    
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var tempImageView: UIImageView!
     
+    //MARK: Drawing variables
     var lastPoint = CGPoint.zero
     var color = UIColor.black
     var brushWidth: CGFloat = 10.0
@@ -23,32 +29,45 @@ class CanvasController: UIViewController {
         super.viewDidLoad()
     }
     
+    //resestPressed() -> a function for clearing the canvas
     @IBAction func resetPressed(_ sender: Any) {
         mainImageView.image = nil
+        tempImageView.image = nil
+        CANVAS_DATA = [:]
+        COLOR = UIColor.black;
     }
     
     
     //To be send pressed
-    @IBAction func sharePressed(_ sender: Any) {
-        guard let image = mainImageView.image else {
-            return
+    @IBAction func sendPressed(_ sender: Any) {
+        
+        for k in CANVAS_DATA.keys {
+            print("the key is...")
+            print(k)
+            print("the values are ...")
+            print(CANVAS_DATA[k])
+            
         }
-        let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        present(activity, animated: true)
+       //guard let image = mainImageView.image else {
+      //      return
+      //  }
+      //  let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+      //  present(activity, animated: true)
     }
     
-    
+    //pencilPressed() -> a function for determing the pencil and color
     @IBAction func pencilPressed(_ sender: UIButton) {
         guard let pencil = Pencil(tag: sender.tag) else {
             return
         }
         color = pencil.color
+        COLOR = pencil.color
         if pencil == .eraser {
             opacity = 1.0
         }
     }
     
-    
+    //touchesBegan() -> records user movement on the canavas.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
             return
@@ -57,50 +76,53 @@ class CanvasController: UIViewController {
         lastPoint = touch.location(in: view)
     }
     
+    //drawLine() -> function that draws line for user
     func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
-        // 1
+        
         UIGraphicsBeginImageContext(view.frame.size)
         guard let context = UIGraphicsGetCurrentContext() else {
             return
         }
         tempImageView.image?.draw(in: view.bounds)
         
-        // 2
         context.move(to: fromPoint)
         context.addLine(to: toPoint)
-        
-        // 3
         context.setLineCap(.round)
         context.setBlendMode(.normal)
         context.setLineWidth(brushWidth)
         context.setStrokeColor(color.cgColor)
-        
-        // 4
         context.strokePath()
         
-        // 5
         tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
         tempImageView.alpha = opacity
         UIGraphicsEndImageContext()
     }
     
+    //touchesMoved() -> a function for recording when touches moved
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
             return
         }
         
-        // 6
         swiped = true
         let currentPoint = touch.location(in: view)
         drawLine(from: lastPoint, to: currentPoint)
-        
-        // 7
         lastPoint = currentPoint
+        
+        //Records canvas point data into dictionary.
+        if CANVAS_DATA[COLOR] != nil {
+            CANVAS_DATA[COLOR]?.append(lastPoint)
+        } else {
+            var point = [CGPoint]()
+            point.append(lastPoint)
+            CANVAS_DATA.updateValue(point, forKey: COLOR)
+        }
+        
     }
     
+    //touchesEneded() -> records when the touches have ended and draws in view
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if !swiped {
-            // draw a single point
             drawLine(from: lastPoint, to: lastPoint)
         }
         
